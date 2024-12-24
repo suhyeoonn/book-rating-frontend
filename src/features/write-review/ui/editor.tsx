85f646e4d0136461fdc61be6b18543d4e88b9aaa
suhyeoonn <suhyeon1740@gmail.com>
2024-12-24 09:56:37 +0900

    💄 에디터 툴바 추가

--- src/features/write-review/ui/editor.tsx
+++ src/features/write-review/ui/editor.tsx
@@ -2,8 +2,8 @@
 
 import { EditorContent } from "@tiptap/react";
 import { Button } from "@/shared/ui/button";
-import EditorMenu from "./editor-menu";
 import { useEditorConfig } from "../config/editor-config";
+import Toolbar from "./editor-toolbar";
 
 export const TiptapEditor = () => {
   const { editor } = useEditorConfig();
@@ -19,13 +19,13 @@ export const TiptapEditor = () => {
 
   return (
     <>
-      <div>
-        <Button onClick={handleSave} size={"sm"}>
+      <Toolbar editor={editor} />
+      <EditorContent editor={editor} />
+      <div className="flex justify-end p-6">
+        <Button onClick={handleSave} size={"lg"}>
           Save
         </Button>
       </div>
-      <EditorMenu editor={editor} />
-      <EditorContent editor={editor} />
     </>
   );
 };
--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -22,8 +22,8 @@ const buttonVariants = cva(
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        lg: "h-11 rounded-md px-8 text-lg",
+        icon: "h-5 w-5",
       },
     },
     defaultVariants: {
