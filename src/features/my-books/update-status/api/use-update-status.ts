8c3e62ebc23bf632a97b8a51ce3d8beab0674dee
suhyeoonn <suhyeon1740@gmail.com>
2024-12-26 13:32:40 +0900

    🩹 상태 갱신 시 캐싱 무효화 처리

--- src/features/my-books/update-status/api/use-update-status.ts
+++ src/features/my-books/update-status/api/use-update-status.ts
@@ -1,14 +1,17 @@
 import { bookQueries, updateStatus } from "@/entities/my-book/api";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 
 export const useUpdateStatus = () => {
   const queryClient = useQueryClient();
+  const router = useRouter();
 
   return useMutation({
     mutationFn: updateStatus,
 
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: bookQueries.list().queryKey });
+      router.refresh();
     },
   });
 };
