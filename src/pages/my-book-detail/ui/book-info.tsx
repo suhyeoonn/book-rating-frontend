50ec2fee0ea20ddd64e1957dd3389e7b7c0aafcf
suhyeoonn <suhyeon1740@gmail.com>
2024-12-31 07:42:02 +0900

    🐛 책 상세페이지 review null 처리

--- src/pages/my-book-detail/ui/book-info.tsx
+++ src/pages/my-book-detail/ui/book-info.tsx
@@ -20,17 +20,17 @@ export default function BookInfo({ book: myBook }: { book: MyBook }) {
   const [open, setOpen] = React.useState(false);
 
   return (
-    <div className="flex gap-16 justify-center text-slate-500">
+    <div className="flex justify-center gap-16 text-slate-500">
       <div className="flex flex-col">
         <Image
           src={validateSrc(book.thumbnail)}
           alt={book.title}
           width={150}
           height={150}
-          className="p-1 max-h-52 object-contain shadow-sm"
+          className="max-h-52 object-contain p-1 shadow-sm"
         />
         <div className="mt-8 flex items-center">
-          {!review.rating ? (
+          {!review?.rating ? (
             <Select open={open} onOpenChange={() => setOpen(true)}>
               <SelectTrigger>
                 <StarGroup rating={0} />
@@ -49,22 +49,22 @@ export default function BookInfo({ book: myBook }: { book: MyBook }) {
 
       <div className="flex-1">
         <div className="flex justify-between"></div>
-        <h1 className="text-2xl lg:text-3xl font-bold text-slate-900">
+        <h1 className="text-2xl font-bold text-slate-900 lg:text-3xl">
           {book.title}
         </h1>
-        <div className="text-sm mt-2 grid grid-cols-2 gap-4 sm:grid-cols-1">
+        <div className="mt-2 grid grid-cols-2 gap-4 text-sm sm:grid-cols-1">
           <DetailItem label="저자">{book.authors}</DetailItem>
           <DetailItem label="출판">
             {book.publisher} | {dayjs(book.datetime).format("YYYY-MM-DD")}
           </DetailItem>
           <BookContent book={book} />
-          <div className="flex gap-1 items-center">
-            <span className="font-semibold pr-1">상태</span>
+          <div className="flex items-center gap-1">
+            <span className="pr-1 font-semibold">상태</span>
             <StatusSelect id={id} value={status} />
           </div>
-          <div className="flex gap-1 items-center">
-            <span className="font-semibold pr-1">한줄평</span>
-            {review.content || "-"}
+          <div className="flex items-center gap-1">
+            <span className="pr-1 font-semibold">한줄평</span>
+            {review?.content || "-"}
           </div>
           <DetailItem label="추가일">{formatDateTime(createdAt)}</DetailItem>
           <DetailItem label="마지막 수정일">
@@ -90,7 +90,7 @@ interface DetailItemProps {
 const DetailItem = ({ label, children }: DetailItemProps) => {
   return (
     <p>
-      <span className="font-semibold pr-3">{label}</span>
+      <span className="pr-3 font-semibold">{label}</span>
       {children}
     </p>
   );
