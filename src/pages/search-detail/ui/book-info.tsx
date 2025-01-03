d89f0e64be9129d2c35c549756adc0ba306bbc0d
suhyeoonn <suhyeon1740@gmail.com>
2025-01-03 16:28:06 +0900

    ♻️ search detail의 book-info 리팩토링
    책 정보와 관련없는 코드를 page 컴포넌트로 이동

--- src/pages/search-detail/ui/book-info.tsx
+++ src/pages/search-detail/ui/book-info.tsx
@@ -1,73 +1,36 @@
 import { Book } from "@/entities/book/types";
-import Image from "next/image";
-import TagGroup from "../../../shared/ui/tag-group";
-import StarGroup from "../../../shared/ui/star-group";
-import { validateSrc } from "@/shared/utils";
 import Link from "next/link";
 import dayjs from "dayjs";
-import { AddMyListButton } from "@/features/my-books/add-my-list";
 
-export default function BookInfo({
-  selectedBook,
-  averageRating,
-}: {
-  selectedBook: Book;
-  averageRating: number;
-}) {
+export default function BookInfo({ selectedBook }: { selectedBook: Book }) {
   return (
-    <div className="flex justify-center gap-16 text-slate-500">
-      <div className="flex flex-col items-center">
-        <Image
-          src={validateSrc(selectedBook.thumbnail)}
-          alt={selectedBook.title}
-          width={150}
-          height={150}
-          className="max-h-52 object-contain p-1"
-        />
-        <div className="flex flex-grow items-center">
-          <AddMyListButton
-            book={{
-              ...selectedBook,
-              tags: [],
-            }}
-          />
-        </div>
-      </div>
-
-      <div className="flex-1">
-        <div className="flex justify-between">
-          <StarGroup rating={averageRating} />
-        </div>
-        <h1 className="mt-4 text-2xl font-bold text-slate-900 lg:text-3xl">
-          {selectedBook.title}
-        </h1>
-        <div className="mt-2 grid grid-cols-2 gap-2 text-sm sm:grid-cols-1">
-          <p>
-            <span className="pr-1 font-semibold">저자</span>
-            {selectedBook.authors}
-          </p>
-          <p>
-            <span className="pr-1 font-semibold">출판</span>
-            {selectedBook.publisher} |{" "}
-            {dayjs(selectedBook.datetime).format("YYYY-MM-DD")}
-          </p>
-        </div>
-        <p className="mt-6 text-sm text-slate-600">
-          {selectedBook.contents + "..."}
-          {selectedBook.url && (
-            <Link
-              target="_blank"
-              href={selectedBook.url}
-              className="text-sm underline hover:text-slate-900"
-            >
-              자세히
-            </Link>
-          )}
+    <>
+      <h1 className="mt-4 text-2xl font-bold text-slate-900 lg:text-3xl">
+        {selectedBook.title}
+      </h1>
+      <div className="mt-2 grid grid-cols-2 gap-2 text-sm sm:grid-cols-1">
+        <p>
+          <span className="pr-1 font-semibold">저자</span>
+          {selectedBook.authors}
         </p>
-        <div className="mt-2 flex flex-wrap gap-2 text-sm text-muted-foreground">
-          <TagGroup tags={selectedBook.tags} />
-        </div>
+        <p>
+          <span className="pr-1 font-semibold">출판</span>
+          {selectedBook.publisher} |{" "}
+          {dayjs(selectedBook.datetime).format("YYYY-MM-DD")}
+        </p>
       </div>
-    </div>
+      <p className="mt-6 text-sm text-slate-600">
+        {selectedBook.contents + "..."}
+        {selectedBook.url && (
+          <Link
+            target="_blank"
+            href={selectedBook.url}
+            className="text-sm underline hover:text-slate-900"
+          >
+            자세히
+          </Link>
+        )}
+      </p>
+    </>
   );
 }
--- src/pages/search-detail/ui/search-detail-page.tsx
+++ src/pages/search-detail/ui/search-detail-page.tsx
@@ -1,20 +1,49 @@
 "use client";
 
 import React from "react";
-import { useState } from "react";
 import BookInfo from "@/pages/search-detail/ui/book-info";
 import ReviewList from "@/pages/search-detail/ui/review-list";
 import { menus } from "@/widgets/layout-header/model/menu";
 import { Breadcrumb } from "@/shared/ui/breadcrumb";
 import { Book } from "@/entities/book/types";
+import Image from "next/image";
+import { validateSrc } from "@/shared/utils";
+import { AddMyListButton } from "@/features/my-books/add-my-list";
+import StarGroup from "@/shared/ui/star-group";
+import TagGroup from "@/shared/ui/tag-group";
 
 export const SearchDetailPage = ({ book }: { book: Book }) => {
-  const [averageRating, setAverageRating] = useState(book?.averageRating || 0);
-
   return (
     <div className="container mx-auto flex flex-col gap-6 px-4 py-8 md:w-3/4 md:px-6 lg:w-1/2">
       <Breadcrumb links={[menus[0]]} pathName={book.title} />
-      <BookInfo selectedBook={book} averageRating={averageRating} />
+      <div className="flex justify-center gap-16 text-slate-500">
+        <div className="flex flex-col items-center">
+          <Image
+            src={validateSrc(book.thumbnail)}
+            alt={book.title}
+            width={150}
+            height={150}
+            className="max-h-52 object-contain p-1"
+          />
+          <div className="flex flex-grow items-center">
+            <AddMyListButton
+              book={{
+                ...book,
+                tags: [],
+              }}
+            />
+          </div>
+        </div>
+        <div className="flex-1">
+          <div className="flex justify-between">
+            <StarGroup rating={book.averageRating} />
+          </div>
+          <BookInfo selectedBook={book} />
+          <div className="mt-2 flex flex-wrap gap-2 text-sm text-muted-foreground">
+            <TagGroup tags={book.tags} />
+          </div>
+        </div>
+      </div>
       <ReviewList selectedBook={book} />
     </div>
   );
