0507e23c9db63ee6f1ea6f4ce4a30f9c5eeb9bd6
suhyeoonn <suhyeon1740@gmail.com>
2025-01-10 01:40:37 +0900

    🩹 useApiError 주석 해제

--- src/app/providers/query-provider.tsx
+++ src/app/providers/query-provider.tsx
@@ -12,10 +12,14 @@ import { ReactNode } from "react";
 import useApiError, { CustomAxiosError } from "./use-api-error";
 import { AxiosError } from "axios";
 
-function makeQueryClient() {
-  // const { handleError } = useApiError();
+export default function QueryProvider({ children }: { children: ReactNode }) {
+  // NOTE: Avoid useState when initializing the query client if you don't
+  //       have a suspense boundary between this and the code that may
+  //       suspend because React will throw away the client on the initial
+  //       render if it suspends and there is no boundary
+  const { handleError } = useApiError();
 
-  return new QueryClient({
+  const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
         // With SSR, we usually want to set some default staleTime
@@ -23,38 +27,14 @@ function makeQueryClient() {
         staleTime: 60 * 1000,
       },
     },
-    // queryCache: new QueryCache({
-    //   onError: (error, query) => {
-    //     if (error instanceof AxiosError) {
-    //       handleError(error as CustomAxiosError); // 명시적으로 CustomAxiosError로 캐스팅
-    //     }
-    //   },
-    // }),
+    queryCache: new QueryCache({
+      onError: (error, query) => {
+        if (error instanceof AxiosError) {
+          handleError(error as CustomAxiosError); // 명시적으로 CustomAxiosError로 캐스팅
+        }
+      },
+    }),
   });
-}
-
-let browserQueryClient: QueryClient | undefined = undefined;
-
-function getQueryClient() {
-  if (isServer) {
-    // Server: always make a new query client
-    return makeQueryClient();
-  } else {
-    // Browser: make a new query client if we don't already have one
-    // This is very important, so we don't re-make a new client if React
-    // suspends during the initial render. This may not be needed if we
-    // have a suspense boundary BELOW the creation of the query client
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
-}
-
-export default function QueryProvider({ children }: { children: ReactNode }) {
-  // NOTE: Avoid useState when initializing the query client if you don't
-  //       have a suspense boundary between this and the code that may
-  //       suspend because React will throw away the client on the initial
-  //       render if it suspends and there is no boundary
-  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
