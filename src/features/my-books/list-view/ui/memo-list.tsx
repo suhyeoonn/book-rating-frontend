3782d3007c0817d5c95623a9a1ab57534d4abf41
suhyeoonn <suhyeon1740@gmail.com>
2025-01-19 17:32:01 +0900

    💄 리스트 메모 작성 슬라이더 추가

--- src/features/my-books/list-view/ui/memo-list.tsx
+++ src/features/my-books/list-view/ui/memo-list.tsx
@@ -1,15 +1,21 @@
 import { cn } from "@/shared/utils";
 import { FileTextIcon, PlusIcon } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
+import { MemoSlider } from "./memo-slider";
+import { Memo } from "../model/memo.interface";
 
-const arr = [
+const arr: Memo[] = [
   {
     id: 1,
     title: "a",
+    createdAt: "2024-12-22 3:00 AM",
+    content: "",
   },
   {
     id: 2,
     title: "b",
+    createdAt: "2024-12-22 3:00 AM",
+    content: "",
   },
 ];
 
@@ -17,20 +23,46 @@ const liStyle =
   "flex cursor-pointer items-center rounded-md p-1 hover:bg-muted/50";
 
 export const MemoList = () => {
+  const [open, setOpen] = useState(false);
+  const [memo, setMemo] = useState<Memo | null>(null);
+
+  const handleCreateMemo = () => {
+    setMemo({
+      id: 100,
+      title: "",
+      createdAt: "2024-12-22 3:00 AM",
+      content: "",
+    });
+    setOpen(true);
+  };
+
+  const handleOpenSlider = (memo: Memo) => {
+    setMemo(memo);
+    setOpen(true);
+  };
+
   return (
-    <ul>
-      {arr.map(({ id, title }) => (
-        <li key={id} className={liStyle}>
-          <FileTextIcon className="mr-1 h-5 w-5 text-slate-500" />
-          {title}
+    <>
+      <ul>
+        {arr.map((memo) => (
+          <li
+            key={memo.id}
+            className={liStyle}
+            onClick={() => handleOpenSlider(memo)}
+          >
+            <FileTextIcon className="mr-1 h-5 w-5 text-slate-500" />
+            {memo.title}
+          </li>
+        ))}
+        <li
+          className={cn(liStyle, "text-sm text-slate-500 hover:text-slate-700")}
+          onClick={handleCreateMemo}
+        >
+          <PlusIcon className="mr-1 h-5 w-5 text-slate-400" />
+          메모 추가
         </li>
-      ))}
-      <li
-        className={cn(liStyle, "text-sm text-slate-500 hover:text-slate-700")}
-      >
-        <PlusIcon className="mr-1 h-5 w-5 text-slate-400" />
-        메모 추가
-      </li>
-    </ul>
+      </ul>
+      {memo && <MemoSlider open={open} setOpen={setOpen} memo={memo} />}
+    </>
   );
 };
---
+++ src/features/my-books/list-view/ui/memo-slider.tsx
@@ -0,0 +1,46 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/shared/ui/sheet";
+import { TiptapEditor } from "../../write-review";
+import { Input } from "@/shared/ui/input";
+import { Memo } from "../model/memo.interface";
+
+interface MemoSliderProps {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  memo: Memo;
+}
+
+export const MemoSlider = ({ open, setOpen, memo }: MemoSliderProps) => {
+  const { title, createdAt, content } = memo;
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen} modal={false}>
+      <SheetContent className="flex flex-col space-y-4">
+        <SheetHeader className="mb-8 space-y-4">
+          <SheetTitle asChild>
+            <Input
+              className="border-0 text-2xl font-bold text-slate-900 shadow-none outline-none placeholder:text-2xl focus:ring-0 lg:text-3xl"
+              value={title}
+              placeholder="제목을 입력하세요"
+              autoFocus
+            />
+          </SheetTitle>
+          <SheetDescription asChild>
+            <div className="grid grid-cols-4 text-sm">
+              <dt>생성일</dt>
+              <dd className="col-span-2">{createdAt}</dd>
+            </div>
+          </SheetDescription>
+        </SheetHeader>
+        <div className="flex-1">
+          <TiptapEditor id={1} memo={content} />
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
