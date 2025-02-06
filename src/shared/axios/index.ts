ca8516d23a258a045dfea9850bbf4463db7587dd
suhyeoonn <suhyeon1740@gmail.com>
2025-02-06 13:16:18 +0900

    🐛 Mixed Content 에러로 인해 프록시로 요청하도록 변경

--- src/shared/axios/index.ts
+++ src/shared/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
